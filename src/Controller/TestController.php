0449d8b8949d4999efb667d2bdc3b8a9faf01541
Farzan <f.behzadian@digikala.com>
2019-09-04 17:11:50 +0430

    New test endpoint

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -3,11 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class TestController
 {
     public function test()
     {
         return new Response('Hello <b>World!</b>');
+    }
+    
+    /**
+     * @Route("/test2")
+     * @return Response
+     */
+    public function test2()
+    {
+        return new Response('Another controller');
     }
 }
